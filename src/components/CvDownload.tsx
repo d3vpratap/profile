c8fdac8c6320bbe4faee60749b4b2faf2c906c09
d3vpratap <singhdevpratap1312@gmail.com>
2025-02-03 23:13:50 +0530

    Updated Cv

--- src/components/CvDownload.tsx
+++ src/components/CvDownload.tsx
@@ -4,7 +4,7 @@ import { Download } from "lucide-react";
 const CvDownload = () => {
   return (
     <a
-      href="https://drive.google.com/uc?export=download&id=1Hk0NnejZ_G9B1sAbf96I9Ic1X15-S-B5" // Direct download link
+      href="https://drive.google.com/file/d/1vVy0bP3pmZRc_SrPzJ_3ht3iSy9eGyst/view?usp=sharing" // Direct download link
       className="fixed bottom-2 right-2 bg-blue-500 p-4 rounded-full shadow-lg hover:bg-blue-600 transition-all duration-300 transform hover:scale-110"
     >
       CV
